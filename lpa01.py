9c51f0a6edae4a8e8e6b9858a3488ca2fb153323
LeonardoElias182 <leonardoelias182@gmail.com>
2024-11-28 13:21:03 -0300

    Add files via upload

---
+++ lpa01.py
@@ -0,0 +1,12 @@
+'*****Digite seus dados*****'
+
+nome = input('Informe seu nome: ')
+
+idade = int(input('Informe sua idade: '))
+
+altura = float(input('Informe sua altura: '))
+
+print (f'Olá, meu nome é {nome}, tenho {idade} anos e tenho {altura} de altura.')
+
+
+
